cfc5f695cfae8b5111fae5e2421bc706b5c52d93
DongHyeonWon <dhe7700@naver.com>
2024-11-21 22:14:41 +0900

    feat: layout 추가

--- src/components/T.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function T({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
-}
---
+++ src/components/_common/Layout/index.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className='flex w-full flex-col items-center overflow-x-hidden bg-gray-500'>
+      <div className='flex h-auto w-full min-w-[32rem] max-w-[48rem] flex-1 flex-col'>
+        <div className='flex w-full flex-col'>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
