46a16d35054c05e47abceb70a5cc256f4771a816
DongHyeonWon <dhe7700@naver.com>
2024-11-25 00:14:07 +0900

    Feature/#93 category tab bar (#3)
    * feat: 공용 컴포넌트 카테고리 탭 임시 구현
    
    * feat: border에 대한 애니메이션 적용
    
    * feat: 주석 추가
    
    * init: tsconfig include 옵션 수정
    
    * Rename: CategoryTab 폴더 생성
    
    ---------
    
    Co-authored-by: rhdmswls12 <rhgusdnr1293@naver.com>

--- src/components/_common/Layout/index.tsx
+++ src/components/_common/Layout/index.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from 'react';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='flex w-full flex-col items-center overflow-x-hidden bg-gray-500'>
-      <div className='flex h-auto w-full min-w-[32rem] max-w-[48rem] flex-1 flex-col'>
+    <div className='flex w-full flex-col items-center overflow-x-hidden bg-gray-100 font-pretendard-variable'>
+      <div className='flex h-auto w-full min-w-[32rem] max-w-[48rem] flex-1 flex-col bg-white'>
         <div className='flex w-full flex-col'>{children}</div>
       </div>
     </div>
