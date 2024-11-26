59bdd90b903498123e6d19edc8c941e9bc87d4e5
khj <ghghjj652@naver.com>
2024-11-26 09:38:19 +0900

    feat: 글로벌 인풋창 구현

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,8 +1,32 @@
+import React, { useState } from 'react';
+
+import Input, { InputValue } from '@/components/_common/Input/Input';
+
 const MainPage = () => {
+  const [val, setVal] = useState<InputValue>('');
+
+  const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setVal(ev.target.value);
+  };
   return (
     <div>
       <div className='text-xl font-bold'>MainPage</div>
       <div className='font-pretendard-variable text-xl'>MainPage</div>
+      <div className='border border-gray-100 p-[2rem]'>
+        <h1 className='text-h2'>인풋창 컴포넌트 구현</h1>
+        <br />
+        <Input
+          // type='number'
+          label='인풋 테스트'
+          id='test'
+          name='test'
+          value={val}
+          onChange={handleChange}
+          placeholder='테스트 값을 입력해주세요'
+        />
+        <br />
+        <Input width='50%' height='5rem' placeholder='label없고 크기조절한 인풋창' />
+      </div>
     </div>
   );
 };
