f3b0f436fc6f29f52c63121d94f4681833c3c50e
fishman <cho.y.j1997@gmail.com>
2024-11-27 11:33:03 +0900

    feat: 지도제외 결제요청 초안완료

--- packages/design-system/components/BorderContainer/BorderContainer.tsx
+++ packages/design-system/components/BorderContainer/BorderContainer.tsx
@@ -7,9 +7,10 @@ interface BorderedContainerProps {
 const BorderContainer: React.FC<BorderedContainerProps> = ({ children }) => {
   return (
     <div className="py-6 px-6 rounded-lg w-full min-w-[300px] bg-secondary">
-      {children}
+      <div className='flex-col rounded-[8px] bg-white p-3'>
+        {children}
+      </div>
     </div>
   );
 };
-
 export default BorderContainer;
