ee086ed1f91bb91af37845e5a62241165956d9e7
DongHyeonWon <dhe7700@naver.com>
2024-11-28 14:41:45 +0900

    불필요한 import 삭제 및 lint 수행 (#16)
    * init: husky 추가
    
    * fix: 불필요한 import 삭제 및 lint 수행

--- apps/owner/src/pages/Request/ProfileViewer.tsx
+++ apps/owner/src/pages/Request/ProfileViewer.tsx
@@ -17,50 +17,50 @@ interface ProfileViewerProps {
 }
 
 const ProfileViewer: React.FC<ProfileViewerProps> = ({
-                                                       profile = {
-                                                         petId: 0,
-                                                         petName: 'Unknown',
-                                                         petImgUrl: '',
-                                                         breed: 'Unknown',
-                                                         birthDate: 'N/A',
-                                                         gender: 'N/A',
-                                                         isNeutered: false,
-                                                         weight: 0,
-                                                         specialNotes: '',
-                                                       },
-                                                     }) => {
+  profile = {
+    petId: 0,
+    petName: 'Unknown',
+    petImgUrl: '',
+    breed: 'Unknown',
+    birthDate: 'N/A',
+    gender: 'N/A',
+    isNeutered: false,
+    weight: 0,
+    specialNotes: '',
+  },
+}) => {
   const { petImgUrl, petName, breed, weight, gender, isNeutered, birthDate } = profile;
 
   return (
-      <div className="rounded-[8px] border-primary p-6 bg-white min-w-[250px] flex justify-center">
-        <div className="flex items-center">
-          <img
-            src={petImgUrl || 'https://via.placeholder.com/100'}
-            alt={petName || 'Unknown Pet'}
-            className="mb-4 h-28 w-28 rounded-full border border-gray-300"
-          />
-          <div className="ml-4 grid grid-cols-2 gap-x-4 gap-y-2">
-            <p className="text-iconCaption text-gray-600">
-              이름: <span className="text-primary">{petName}</span>
-            </p>
-            <p className="text-iconCaption  text-gray-600">
-              견종: <span className="text-primary">{breed}</span>
-            </p>
-            <p className="text-iconCaption text-gray-600">
-              몸무게: <span className="text-primary">{weight}kg</span>
-            </p>
-            <p className="text-iconCaption text-gray-600">
-              성별: <span className="text-primary">{gender === 'male' ? '수컷' : '암컷'}</span>
-            </p>
-            <p className="text-iconCaption text-gray-600">
-              중성화 여부: <span className="text-primary">{isNeutered ? '예' : '아니오'}</span>
-            </p>
-            <p className="text-iconCaption text-gray-600">
-              출생일: <span className="text-primary">{birthDate}</span>
-            </p>
-          </div>
+    <div className='flex min-w-[250px] justify-center rounded-[8px] border-primary bg-white p-6'>
+      <div className='flex items-center'>
+        <img
+          src={petImgUrl || 'https://via.placeholder.com/100'}
+          alt={petName || 'Unknown Pet'}
+          className='mb-4 h-28 w-28 rounded-full border border-gray-300'
+        />
+        <div className='ml-4 grid grid-cols-2 gap-x-4 gap-y-2'>
+          <p className='text-iconCaption text-gray-600'>
+            이름: <span className='text-primary'>{petName}</span>
+          </p>
+          <p className='text-iconCaption text-gray-600'>
+            견종: <span className='text-primary'>{breed}</span>
+          </p>
+          <p className='text-iconCaption text-gray-600'>
+            몸무게: <span className='text-primary'>{weight}kg</span>
+          </p>
+          <p className='text-iconCaption text-gray-600'>
+            성별: <span className='text-primary'>{gender === 'male' ? '수컷' : '암컷'}</span>
+          </p>
+          <p className='text-iconCaption text-gray-600'>
+            중성화 여부: <span className='text-primary'>{isNeutered ? '예' : '아니오'}</span>
+          </p>
+          <p className='text-iconCaption text-gray-600'>
+            출생일: <span className='text-primary'>{birthDate}</span>
+          </p>
         </div>
       </div>
+    </div>
   );
 };
 
