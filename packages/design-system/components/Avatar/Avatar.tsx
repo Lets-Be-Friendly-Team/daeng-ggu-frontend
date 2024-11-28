be528f7115f9a46e7a1fd154e48e315c1a39d1fe
rhdmswls12 <rhgusdnr1293@naver.com>
2024-11-28 14:28:34 +0900

    feat: plus 아이콘 추가

---
+++ packages/design-system/components/Avatar/Avatar.tsx
@@ -0,0 +1,11 @@
+interface IAvatarProps {
+  imageUrl?: string;
+  text?: string;
+  isActive?: boolean;
+  onClick?: () => void;
+}
+function Avatar() {
+  return <div></div>;
+}
+
+export default Avatar;
