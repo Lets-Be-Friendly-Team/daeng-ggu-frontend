1d79b45ba290b446fb00b0351030c9002bd3058b
rhdmswls12 <rhgusdnr1293@naver.com>
2024-11-30 13:24:11 +0900

    fix: 메인 페이지 수정

--- apps/owner/src/pages/MainPage/MainPage.tsx
+++ apps/owner/src/pages/MainPage/MainPage.tsx
@@ -2,14 +2,11 @@
 
 // import OwnerBottomTabBar from '@/components/BottomTabBar/OwnerBottomTabBar';
 
-import { ServiceCheckBox } from '@daeng-ggu/design-system';
-
 const MainPage = () => {
   return (
     <div>
       <div className='text-xl font-bold'>MainPagee</div>
       <div className='font-pretendard-variable text-xl'></div>
-      <ServiceCheckBox />
     </div>
   );
 };
