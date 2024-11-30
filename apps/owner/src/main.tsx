24db751e4a3259a794cf6b098306d73b1d326302
DongHyeonWon <dhe7700@naver.com>
2024-11-30 11:41:38 +0900

    yarn.lock 업데이트 (#33)
    * init: 불필요한 의존성 삭제
    
    * chore: 디자인 시스템 상대경로로 수정
    
    * chore: yarn.lock 수정
    
    * feat: Calendar 컴포넌트 추가
    
    * feat: 스토리북 layout 추가
    
    * feat: 폰트 설정 추가
    
    * feat: 공통 컴포넌트 스토리북 추가
    
    * refac: React.FC 삭제
    
    * chore: yarn.lock 업데이트
    
    * fix: BrowserRouter 추가

--- apps/owner/src/main.tsx
+++ apps/owner/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -14,8 +14,10 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
 );
