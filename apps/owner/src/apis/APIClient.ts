f239c5610d4da87055e8fd1c2c2c95964de7855c
DongHyeonWon <dhe7700@naver.com>
2024-12-02 16:30:20 +0900

    feat: APIclient 추가

---
+++ apps/owner/src/apis/APIClient.ts
@@ -0,0 +1,9 @@
+import APIFetch from '@/apis/APIFetch';
+
+export const API_URL = import.meta.env.VITE_BASE_URL;
+
+const header = new Headers({
+  'Content-Type': 'application/json',
+});
+
+export const APIClient = new APIFetch(API_URL, header);
