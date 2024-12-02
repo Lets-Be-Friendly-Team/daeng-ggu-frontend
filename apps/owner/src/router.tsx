eb56ee9a2661ef3a9df96ca2829b8357c14736a9
fishman <cho.y.j1997@gmail.com>
2024-12-02 21:28:18 +0900

    feat: 견적 라우터 수정

--- apps/owner/src/router.tsx
+++ apps/owner/src/router.tsx
@@ -28,7 +28,7 @@ export const router = createBrowserRouter([
         element: <MainPage />,
       },
       {
-        path: 'bid',
+        path: ROUTES.bid,
         children: [
           {
             index: true,
