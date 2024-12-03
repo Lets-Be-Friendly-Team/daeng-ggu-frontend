dc35ab814d535a7541a0ace790520d9dd1cb6928
fishman <cho.y.j1997@gmail.com>
2024-12-03 22:30:49 +0900

    fix: 센트리 임시 주석처리

--- apps/owner/src/sentry.ts
+++ apps/owner/src/sentry.ts
@@ -1,17 +1,17 @@
-import React from 'react';
-import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-router-dom';
-import * as Sentry from '@sentry/react';
-
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect: React.useEffect,
-      useLocation,
-      useNavigationType,
-      createRoutesFromChildren,
-      matchRoutes,
-    }),
-  ],
-  tracesSampleRate: 1.0,
-});
+// import React from 'react';
+// import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-router-dom';
+// import * as Sentry from '@sentry/react';
+//
+// Sentry.init({
+//   dsn: import.meta.env.VITE_SENTRY_DSN,
+//   integrations: [
+//     Sentry.reactRouterV6BrowserTracingIntegration({
+//       useEffect: React.useEffect,
+//       useLocation,
+//       useNavigationType,
+//       createRoutesFromChildren,
+//       matchRoutes,
+//     }),
+//   ],
+//   tracesSampleRate: 1.0,
+// });
