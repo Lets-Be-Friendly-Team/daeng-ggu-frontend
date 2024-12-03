e8d274586c4a00869cd09cd9f266c1670eb00260
khj <ghghjj652@naver.com>
2024-12-03 16:42:05 +0900

    feat: 프리미엄 서비스 종류별 조회 ui 구현

--- apps/owner/src/components/DesignerInfo/PremiumList.tsx
+++ apps/owner/src/components/DesignerInfo/PremiumList.tsx
@@ -1,6 +1,19 @@
+import { CategoryTab } from '@daeng-ggu/design-system';
+
+import DesignerList from './DesignerList';
+
 // 제공하는 프리미엄 서비스별로 조회하기 위한 컴포넌트
 const PremiumList = () => {
-  return <div></div>;
+  const tabs = [
+    { label: '스파', content: <DesignerList category='spa' /> },
+    { label: '풀케어', content: <DesignerList category='fullcare' /> },
+    { label: '스트리밍', content: <DesignerList category='streaming' /> },
+  ];
+  return (
+    <div className='mt-[-2rem]'>
+      <CategoryTab tabs={tabs} />
+    </div>
+  );
 };
 
 export default PremiumList;
