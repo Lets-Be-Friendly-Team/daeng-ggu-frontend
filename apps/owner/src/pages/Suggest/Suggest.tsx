e0ff5b8df23a9da696d4d81121259709c138a200
fishman <cho.y.j1997@gmail.com>
2024-12-06 01:34:49 +0900

    feat: 캘린더 디자인 수정 및 견적제안 초안 구현

--- apps/owner/src/pages/Suggest/Suggest.tsx
+++ apps/owner/src/pages/Suggest/Suggest.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { BorderContainer, Calendar, Header, TimeSelect } from '@daeng-ggu/design-system';
+import { BorderContainer, Calendar, Header, TimeSelect, TypeOneButton } from '@daeng-ggu/design-system';
 import { format } from 'date-fns';
 
 import TextEditor from '@/pages/Suggest/TextEditor.tsx';
@@ -11,26 +11,29 @@ const Suggest = () => {
   const location = useLocation();
   const { address, customerName, desiredDateOne, desiredDateTwo, desiredDateThree, phone } = location.state || {};
 
-  console.log(desiredDateOne, desiredDateTwo, desiredDateThree);
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [selectedTime, setSelectedTime] = useState<number | null>(null);
+  const [showTimeSelect, setShowTimeSelect] = useState(false); // State to control visibility
 
-  // Parse desired dates into Date objects
   const desiredDates = [desiredDateOne, desiredDateTwo, desiredDateThree]
     .filter(Boolean)
     .map((dateString) => new Date(dateString));
 
-  // Extract unique dates (without time)
   const desiredDatesSet = new Set(desiredDates.map((date) => format(date, 'yyyy-MM-dd')));
 
-  // Handle date selection
   const handleDateClick = (date: Date | undefined) => {
     if (!date) return;
     const selectedDateString = format(date, 'yyyy-MM-dd');
-    if (!desiredDatesSet.has(selectedDateString)) return; // Ignore dates that are not desired
+    if (!desiredDatesSet.has(selectedDateString)) return;
 
-    setSelectedDate(date);
-    setSelectedTime(null); // Reset selected time when date changes
+    if (selectedDate && format(selectedDate, 'yyyy-MM-dd') === selectedDateString) {
+      setSelectedDate(null);
+      setShowTimeSelect(false);
+    } else {
+      setSelectedDate(date);
+      setSelectedTime(null);
+      setShowTimeSelect(true);
+    }
   };
 
   // Handle time selection
@@ -66,29 +69,55 @@ const Suggest = () => {
           <div className='items-start'>
             <h2 className='mb-4 text-h3 font-bold text-gray-800'>견적서 설정</h2>
           </div>
-          <div className='mb-40 h-full w-full'>
-            <BorderContainer innerPadding='p-3'>
-              <Calendar
-                mode='single'
-                selected={selectedDate || undefined}
-                onSelect={handleDateClick}
-                disabled={(date) => {
-                  const dateString = format(date, 'yyyy-MM-dd');
-                  return !desiredDatesSet.has(dateString);
-                }}
-              />
-            </BorderContainer>
-            {selectedDate && (
-              <div className='mt-4'>
-                <TimeSelect
-                  availableTimes={getAvailableTimes()}
-                  selectValue={selectedTime}
-                  onSelectChange={handleTimeSelectChange}
-                />
+          <div className='mb-14 h-full w-full'>
+            <BorderContainer>
+              <div className='w-full bg-secondary'>
+                <div>
+                  <div className='rounded-[8px] bg-white'>
+                    <Calendar
+                      mode='single'
+                      selected={selectedDate || undefined}
+                      onSelect={handleDateClick}
+                      disabled={(date) => {
+                        const dateString = format(date, 'yyyy-MM-dd');
+                        return !desiredDatesSet.has(dateString);
+                      }}
+                    />
+                  </div>
+                </div>
+                <div
+                  className={`mt-4 overflow-hidden transition-all duration-300 ease-in-out ${
+                    showTimeSelect ? 'max-h-[1000px] opacity-100' : 'max-h-0 opacity-0'
+                  }`}
+                >
+                  <div className='w-full bg-secondary'>
+                    <div className='flex justify-center rounded-[8px] bg-white py-4'>
+                      <TimeSelect
+                        availableTimes={getAvailableTimes()}
+                        selectValue={selectedTime}
+                        onSelectChange={handleTimeSelectChange}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className='mt-6 w-full bg-secondary'>
+                  <div>
+                    <div className='flex flex-col justify-center rounded-[8px] bg-white py-4 pl-6'>
+                      <div className='text-sub_h2 font-bold'>가격입력</div>
+                      <div className='mt-3 flex items-center text-sub_h1'>
+                        <div>
+                          <input className='w-full border-b focus:outline-none' placeholder='₩ 가격' type='number' />
+                        </div>
+                        <span className='ml-2'>원</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            )}
+            </BorderContainer>
           </div>
-          <div className='mb-52 w-full'>
+
+          <div className='mb-56 w-full'>
             <div className='mt-6 items-start'>
               <h2 className='mb-4 text-h3 font-bold text-gray-800'>댕송지 정보</h2>
             </div>
@@ -100,6 +129,9 @@ const Suggest = () => {
               </div>
             </BorderContainer>
           </div>
+        </div>
+        <div className='fixed w-full' style={{ bottom: '65px' }}>
+          <TypeOneButton text='제안하기' color='bg-secondary' onClick={() => {}} />
         </div>
       </div>
     </div>
--- packages/design-system/components/Calendar/Calendar.tsx
+++ packages/design-system/components/Calendar/Calendar.tsx
@@ -1,50 +1,55 @@
 import { ComponentProps } from 'react';
-import { DayPicker } from 'react-day-picker';
+import { DayContentProps, DayPicker } from 'react-day-picker';
+import { isToday } from 'date-fns';
 import { ko } from 'date-fns/locale';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 import { cn } from '../../lib/utils';
 
-import { buttonVariants } from './buttonVariants';
-
 export type CalendarProps = ComponentProps<typeof DayPicker>;
 
 function Calendar({ className, classNames, showOutsideDays = true, ...props }: CalendarProps) {
+  // Custom DayContent component
+  function CustomDayContent(dayContentProps: DayContentProps) {
+    const { date } = dayContentProps;
+    const dayNumber = date.getDate();
+    const isCurrentDay = isToday(date);
+
+    return (
+      <div className={cn('flex flex-col items-center justify-center', className)}>
+        <div>{dayNumber}</div>
+        {isCurrentDay && <div className='text-xs'>오늘</div>}
+      </div>
+    );
+  }
+
   return (
     <DayPicker
       locale={ko}
       showOutsideDays={showOutsideDays}
       className={cn('p-3 font-pretendard', className)}
       classNames={{
-        // Keeping head styles unchanged
         months: 'flex flex-row h-full',
         month: 'space-y-4 w-full',
-        caption: 'flex justify-center pt-1 relative items-center text-lg',
+        caption: 'relative flex justify-center items-center pt-1 text-lg',
         caption_label: 'text-[2rem] font-bold',
-        nav: 'space-x-1 flex items-center',
-        nav_button: cn(buttonVariants({ variant: 'outline' }), 'bg-transparent p-6 opacity-50 hover:opacity-100'),
-        nav_button_previous: 'absolute left-1 w-[2rem] h-[2rem]',
-        nav_button_next: 'absolute right-1 w-[2rem] h-[2rem]',
+        nav: '',
+        nav_button: cn('bg-transparent p-2 opacity-50 hover:opacity-100'),
+        nav_button_previous: 'absolute left-0 top-1/2 transform -translate-y-1/2',
+        nav_button_next: 'absolute right-0 top-1/2 transform -translate-y-1/2',
         table: 'w-full border-collapse',
         head_row: 'flex w-full justify-between mt-8',
         head_cell: 'text-[2rem] w-full flex justify-center text-gray-800 rounded-md font-regular',
-        // Adjusting the date part for dynamic gaps
-        row: 'flex mt-4 justify-between', // Changed justify-center to justify-between
-        cell: cn(
-          'relative flex-1 p-2 sm:p-2 md:p-4 text-center',
-          'focus-within:relative focus-within:z-20',
-          '[&:has([aria-selected])]:bg-white',
-          '[&:has([aria-selected].day-outside)]:bg-white',
-          '[&:has([aria-selected].day-range-end)]:rounded-r-md',
-        ),
+        row: 'flex mt-4',
+        cell: cn('relative', 'w-[calc(100%/7)]', 'flex items-center justify-center', 'p-0.5'),
         day: cn(
-          buttonVariants({ variant: 'ghost' }),
-          'w-full h-full text-[1.6rem] p-0 font-regular',
+          'w-full aspect-square text-[1.6rem] font-regular',
+          'flex flex-col items-center justify-center',
+          'focus:outline-none',
+          'disabled:text-gray-400',
           'aria-selected:opacity-100 day_outside:text-gray-200',
+          'rounded-[8px]',
         ),
-        // Keeping other day styles unchanged
-        day_range_start: 'day-range-start',
-        day_range_end: 'day-range-end',
         day_selected:
           'bg-primary text-white hover:bg-primary hover:text-secondary focus:bg-primary focus:text-secondary',
         day_today: 'bg-secondary text-black aria-selected:bg-primary aria-selected:text-white',
@@ -54,8 +59,9 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
         ...classNames,
       }}
       components={{
-        IconLeft: () => <ChevronLeft className='h-4 w-4' />,
-        IconRight: () => <ChevronRight className='h-4 w-4' />,
+        IconLeft: () => <ChevronLeft className='h-6 w-6' />,
+        IconRight: () => <ChevronRight className='h-6 w-6' />,
+        DayContent: CustomDayContent,
       }}
       {...props}
     />
