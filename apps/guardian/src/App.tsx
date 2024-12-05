ff21e17591b9b396b80bd84841f8501532c4b7f3
DongHyeonWon <dhe7700@naver.com>
2024-12-05 19:11:51 +0900

    feat: designer, guardian Provider 추가

--- apps/guardian/src/App.tsx
+++ apps/guardian/src/App.tsx
@@ -1,12 +1,35 @@
+import { useState } from 'react';
+import { NavermapsProvider } from 'react-naver-maps';
 import { Outlet } from 'react-router';
-import { Layout } from '@daeng-ggu/design-system';
+import { Layout, ModalContainer, ToastContainer } from '@daeng-ggu/design-system';
+import { useToast } from '@daeng-ggu/shared';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import DesignerBottomTabBar from '@/components/BottomTabBar/DesignerBottomTabBar.tsx';
 function App() {
+  const { showToast } = useToast();
+
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          mutations: { onError: (error) => showToast({ message: error.message, type: 'error' }) },
+          queries: { throwOnError: true },
+        },
+      }),
+  );
   return (
-    <Layout tab={<DesignerBottomTabBar />}>
-      <Outlet />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <NavermapsProvider ncpClientId={import.meta.env.VITE_NAVER_CLIENT_ID}>
+        <Layout tab={<DesignerBottomTabBar />}>
+          <ToastContainer />
+          <ModalContainer />
+          <Outlet />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Layout>
+      </NavermapsProvider>
+    </QueryClientProvider>
   );
 }
 
