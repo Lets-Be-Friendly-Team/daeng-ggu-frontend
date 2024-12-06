47e3b932c6e7a764e050c3f141e88261e7b8728b
DongHyeonWon <dhe7700@naver.com>
2024-12-06 16:35:45 +0900

    feat: StarRating 0.5점 단위 추가

--- packages/design-system/components/StarRating/StarRating.tsx
+++ packages/design-system/components/StarRating/StarRating.tsx
@@ -1,43 +1,65 @@
 import { useState } from 'react';
 
+import { cn } from '../../lib/utils';
+import HalfStarIcon from '../Icons/HalfStarIcon';
 import StarIcon from '../Icons/StarIcon';
 
 const StarRating = ({
-  ratingState,
-  handleOnClick,
+  className,
   maxStars = 5,
+  ratingState,
+  setRatingState,
 }: {
-  ratingState: number;
-  handleOnClick: (_index: number) => void;
+  className?: string;
   maxStars?: number;
+  ratingState: number;
+  setRatingState: (_newRating: number) => void;
 }) => {
   const [hoverRating, setHoverRating] = useState<number | null>(null); // 호버 시 임시 점수, null로 초기화
 
-  // 별에 마우스를 올렸을 때의 동작
-  const handleMouseOver = (index: number) => {
-    if (index + 1 > ratingState) {
-      setHoverRating(index + 1); // 클릭한 점수보다 큰 경우에만 업데이트
-    } else {
-      setHoverRating(null); // 작거나 같으면 hover 상태를 무시
-    }
+  const handleClick = (index: number, isHalf: boolean) => {
+    const newRating = index + (isHalf ? 0.5 : 1); // 반 클릭이면 0.5 추가
+    setRatingState(newRating);
   };
 
-  // 마우스를 별에서 뗐을 때의 동작
+  const handleMouseOver = (index: number, isHalf: boolean) => {
+    const newHoverRating = index + (isHalf ? 0.5 : 1); // 반 호버이면 0.5 추가
+    setHoverRating(newHoverRating);
+  };
+
   const handleMouseLeave = () => {
     setHoverRating(null); // 호버 점수 초기화
   };
 
   return (
-    <div className='flex space-x-1'>
-      {Array.from({ length: maxStars }, (_, index) => (
-        <StarIcon
-          key={index}
-          onClick={() => handleOnClick(index)}
-          onMouseOver={() => handleMouseOver(index)}
-          onMouseLeave={handleMouseLeave}
-          isSelect={index < (hoverRating ?? ratingState)} // 호버 점수가 null이면 클릭 점수 사용
-        />
-      ))}
+    <div className={cn(className)}>
+      <span className='text-sub_h1 font-semibold text-gray-800'>별점</span>
+      <div className='flex space-x-1'>
+        {Array.from({ length: maxStars }, (_, index) => {
+          const currentRating = hoverRating ?? ratingState;
+
+          const isFullSelected = index + 1 <= currentRating; // 전체 별이 선택되었는지
+          const isHalfSelected = !isFullSelected && index + 0.5 <= currentRating; // 반별이 선택되었는지
+
+          return (
+            <div key={index} className='relative'>
+              {isFullSelected ? <StarIcon isSelect /> : isHalfSelected ? <HalfStarIcon /> : <StarIcon />}
+              <div
+                className='absolute left-0 top-0 z-10 h-full w-1/2 cursor-pointer'
+                onClick={() => handleClick(index, true)}
+                onMouseOver={() => handleMouseOver(index, true)}
+                onMouseLeave={handleMouseLeave}
+              />
+              <div
+                className='absolute right-0 top-0 z-10 h-full w-1/2 cursor-pointer'
+                onClick={() => handleClick(index, false)}
+                onMouseOver={() => handleMouseOver(index, false)}
+                onMouseLeave={handleMouseLeave}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
