8c199ff24704952c03d9308ed17919d99d8fc369
DongHyeonWon <dhe7700@naver.com>
2024-12-07 14:30:46 +0900

    Layout 스크롤바 제거 (#66)
    * feat: map marker 추가
    
    * style: 뒤로가기, 닫기 icon 변경
    
    * fix: 속성명 수정
    
    * feat: 유저 위치 정보 획득
    
    * fix: Routes 수정
    
    * fix: CloseIcon 속성 수정
    
    * feat: 레이아웃 scrollbar-hide

--- packages/design-system/components/Layout/Layout.tsx
+++ packages/design-system/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 const Layout = ({ children, tab }: { children: ReactNode; tab?: ReactNode }) => {
   return (
-    <div className='flex h-[100vh] w-full flex-col items-center overflow-x-hidden bg-gray-400 font-pretendard-variable'>
+    <div className='flex h-[100vh] w-full flex-col items-center overflow-x-hidden bg-gray-400 font-pretendard-variable scrollbar-hide'>
       <div className='flex w-full min-w-[32rem] max-w-[48rem] flex-1 flex-col bg-white'>
         <div className='flex w-full flex-col'>{children}</div>
         {tab}
