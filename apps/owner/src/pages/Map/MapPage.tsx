2c44f501d970f712bd05d6c72770d5c1a73776ab
DongHyeonWon <dhe7700@naver.com>
2024-12-07 10:29:37 +0900

    feat: map marker 추가

--- apps/owner/src/pages/Map/MapPage.tsx
+++ apps/owner/src/pages/Map/MapPage.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useState } from 'react';
-import { Marker, NavermapsProvider } from 'react-naver-maps';
 import { Header, SearchBar } from '@daeng-ggu/design-system';
 
+import CustomMarker from '@/components/NaverMap/CustomMarker';
 import NaverMapContent from '@/components/NaverMap/NaverMapContent';
 
 const MapPage = () => {
@@ -16,26 +16,30 @@ const MapPage = () => {
   };
 
   return (
-    <NavermapsProvider ncpClientId='j6dpvzog6c'>
-      <div className='relative'>
-        {/* <div className='px-8'>
+    <div className='relative'>
+      {/* <div className='px-8'>
           <Header mode='back' title='지도로 검색' />
         </div> */}
-        <div className='absolute z-10 w-full'>
-          <div className='bg-white px-8'>
-            <Header mode='back' title='지도로 검색' />
-          </div>
-          <div className='p-8'>
-            <SearchBar keyword={keyword} onChange={handleKeywordChange} handleSearch={handleSearch} />
-          </div>
+      <div className='absolute z-10 w-full'>
+        <div className='bg-white px-8'>
+          <Header mode='back' title='지도로 검색' />
         </div>
-        {/* <div className='absolute top-0 mb-[6.5rem] h-[calc(100vh-50px)]'> */}
-        <NaverMapContent className='absolute top-0 h-[calc(100vh-65px)]' mapLat={37.3595704} mapLng={127.105399}>
-          <Marker position={{ lat: 37.3595704, lng: 127.105399 }} />
-        </NaverMapContent>
-        {/* </div> */}
+        <div className='p-8'>
+          <SearchBar keyword={keyword} onChange={handleKeywordChange} handleSearch={handleSearch} />
+        </div>
       </div>
-    </NavermapsProvider>
+      {/* <div className='absolute top-0 mb-[6.5rem] h-[calc(100vh-50px)]'> */}
+      <NaverMapContent className='absolute top-0 h-[calc(100vh-65px)]' mapLat={37.3595704} mapLng={127.105399}>
+        <CustomMarker
+          markerList={[
+            { designerId: 1, lat: 37.3595704, lng: 127.105399, nickname: 'test' },
+            { designerId: 2, lat: 35.3595704, lng: 122.105399, nickname: 'test' },
+            { designerId: 3, lat: 36.3595704, lng: 125.105399, nickname: 'test' },
+          ]}
+        />
+      </NaverMapContent>
+      {/* </div> */}
+    </div>
   );
 };
 
