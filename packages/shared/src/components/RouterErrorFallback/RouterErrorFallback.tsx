628330396f7a99b25fdb6ccb78a3c018b6cb0fed
DongHyeonWon <dhe7700@naver.com>
2024-12-08 21:05:26 +0900

    style: RouteErrorFallback ui 수정

--- packages/shared/src/components/RouterErrorFallback/RouterErrorFallback.tsx
+++ packages/shared/src/components/RouterErrorFallback/RouterErrorFallback.tsx
@@ -6,10 +6,10 @@ const RouterErrorFallback = () => {
   };
   return (
     <section className='flex h-[80vh] w-full flex-col items-center justify-center gap-[2rem] font-pretendard'>
-      <img className='mb-[2rem] h-[15rem]' alt='logo' src={LogoImage} />
+      <img className='mb-[2rem] h-[10rem]' alt='logo' src={LogoImage} />
       <h1 className='text-h1 font-bold'>페이지 전환 시 에러가 발생했어요!</h1>
-      <span className=' text-sub_h2'>다시 접속해주세요!</span>
-      <TypeTwoButton color='bg-primary' onClick={goMain} text='홈으로 가기' />
+      <span className=' text-sub_h1'>다시 접속해주세요!</span>
+      <TypeTwoButton className='w-[40rem]' color='bg-primary' onClick={goMain} text='홈으로 가기' />
     </section>
   );
 };
