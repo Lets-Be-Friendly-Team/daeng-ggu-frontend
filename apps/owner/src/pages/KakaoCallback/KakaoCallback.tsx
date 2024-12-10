75835b623054d22d42469783e2d0343b23f884b7
DongHyeonWon <dhe7700@naver.com>
2024-12-10 20:39:47 +0900

    카카오 로그인 추가 (#87)
    * fix: clipPath로 수정
    
    * refac: modal onClose 수정
    
    * feat: github action env 설정
    
    * feat: naver map 기능 수정
    
    * feat: 카카오 로그인 추가

--- apps/owner/src/pages/KakaoCallback/KakaoCallback.tsx
+++ apps/owner/src/pages/KakaoCallback/KakaoCallback.tsx
@@ -1,12 +1,19 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 
+import getUserInfo from '@/apis/login/GetUserInfo';
 import ROUTES from '@/constants/routes';
 
 const KakaoCallback = () => {
   const navigate = useNavigate();
+
   useEffect(() => {
     // 추후 사용자 정보 가져오기 API 연동 시 사용
+    const userInfoHandle = async () => {
+      console.log(await getUserInfo());
+      // 추후 회원가입 페이지로 갈지 홈으로 갈지 분기처리
+    };
+    userInfoHandle();
     navigate(ROUTES.main);
   }, [navigate]);
   return <></>;
--- apps/owner/vite.config.ts
+++ apps/owner/vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+// import mkcert from 'vite-plugin-mkcert';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
