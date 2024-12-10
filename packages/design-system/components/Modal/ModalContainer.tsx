29a33a0f7b6931c6d7e707a406eb3f872470e335
DongHyeonWon <dhe7700@naver.com>
2024-12-10 13:10:02 +0900

    github action env 설정 및 modal onClose 수정 (#84)
    * fix: clipPath로 수정
    
    * refac: modal onClose 수정
    
    * feat: github action env 설정

--- packages/design-system/components/Modal/ModalContainer.tsx
+++ packages/design-system/components/Modal/ModalContainer.tsx
@@ -5,9 +5,14 @@ import { ModalProps } from '../../types/modal';
 const ModalContainer = () => {
   const { Component, props, isOpen, close } = useModalStore();
   const p = props as ModalProps;
+  const handleOnClose = () => {
+    p?.onClose();
+    close();
+  };
+
   if (!isOpen || !Component) return null;
 
-  return <Component {...p} onClose={close} />;
+  return <Component {...p} onClose={handleOnClose} />;
 };
 
 export default ModalContainer;
