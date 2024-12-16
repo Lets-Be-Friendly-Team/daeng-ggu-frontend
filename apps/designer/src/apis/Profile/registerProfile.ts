0eae3d6154266f70c270d2c7aa4c40bffef0bc4c
khj <ghghjj652@naver.com>
2024-12-16 21:54:37 +0900

    feat: 디자이너 프로필 등록 api 연동 초기

---
+++ apps/designer/src/apis/Profile/registerProfile.ts
@@ -0,0 +1,15 @@
+import { APIClient } from '@daeng-ggu/shared';
+
+import { DesignerData } from '@/pages/RegisterProfile/RegisterProfileData';
+
+export interface RegisterProfileResponse {
+  status: string;
+  message: string;
+  //   data: { petId: number };
+}
+
+const registerProfile = async (profileData: DesignerData): Promise<RegisterProfileResponse> => {
+  return await APIClient.post('/daengggu/designer/register/profile', profileData);
+};
+
+export default registerProfile;
---
+++ apps/designer/src/hooks/queries/Profile/useRegisterProfile.ts
@@ -0,0 +1,21 @@
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+
+import registerProfile, { RegisterProfileResponse } from '@/apis/Profile/RegisterProfile';
+import { DesignerData } from '@/pages/RegisterProfile/RegisterProfileData';
+
+interface RegisterProfileError {
+  message: string;
+  status: number;
+  code: number;
+}
+
+const useRegisterProfile = (
+  mutationOptions?: UseMutationOptions<RegisterProfileResponse, RegisterProfileError, DesignerData>,
+) => {
+  return useMutation({
+    mutationFn: async (profileData: DesignerData) => await registerProfile(profileData),
+    ...mutationOptions,
+  });
+};
+
+export default useRegisterProfile;
