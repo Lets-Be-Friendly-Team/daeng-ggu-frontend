c38c1f567e8ef7862b9f98702c800c470aeb4065
DongHyeonWon <dhe7700@naver.com>
2024-12-19 17:38:48 +0900

    feat: 견적 요청 페이지 캐시 무효화 추가

--- apps/owner/src/hooks/queries/Request/useCreateBidRequest.ts
+++ apps/owner/src/hooks/queries/Request/useCreateBidRequest.ts
@@ -1,8 +1,10 @@
 // src/hooks/useCreateBidRequest.ts
 
-import { useMutation, UseMutationResult } from '@tanstack/react-query';
+import { useMutation, UseMutationResult, useQueryClient } from '@tanstack/react-query';
 
 import { BidRequestData, BidRequestResponse, postCreateBidRequest } from '@/apis/request/postCreateBidRequest';
+import { PENDING_REQUESTS_QUERY_KEY } from '@/hooks/queries/Request/useGetPendingRequests';
+import { USER_COMPLETED_REQUESTS_QUERY_KEY } from '@/hooks/queries/Request/useGetUserCompletedRequests';
 // import { useToast } from '@daeng-ggu/shared'; // Corrected import path
 
 /**
@@ -11,12 +13,15 @@ import { BidRequestData, BidRequestResponse, postCreateBidRequest } from '@/apis
  * @returns A mutation object containing mutate function and mutation state.
  */
 const useCreateBidRequest = (): UseMutationResult<BidRequestResponse, Error, BidRequestData> => {
+  const queryClient = useQueryClient();
   // const { showToast } = useToast();
   return useMutation<BidRequestResponse, Error, BidRequestData>({
     mutationFn: (data: BidRequestData) => postCreateBidRequest(data),
     onSuccess: (data) => {
       // Optional: Handle success globally, e.g., show a toast notification
       console.log('Bid request created successfully:', data);
+      queryClient.invalidateQueries({ queryKey: PENDING_REQUESTS_QUERY_KEY });
+      queryClient.invalidateQueries({ queryKey: USER_COMPLETED_REQUESTS_QUERY_KEY });
     },
     onError: (error) => {
       // Optional: Handle errors globally
